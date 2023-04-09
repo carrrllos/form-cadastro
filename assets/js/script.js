a46e2141a2208efc876c04defd3c33f084fd3a9b
carrrllos <caedu762@gmail.com>
2023-04-09 17:00:33 -0300

    full frame -database

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+
+
+const loginLink = document.getElementById("login-link")
+const registerLink = document.getElementById("register-link")
+
+
+//mudança de login para register
+
+registerLink.addEventListener("click", () => {
+    let loginSection = document.getElementById("login-section")
+    loginSection.style.display = "none"
+    let registerSection = document.getElementById("register-section")
+    registerSection.style.display = "flex"
+
+})
+loginLink.addEventListener("click", () => {
+    let loginSection = document.getElementById("login-section")
+    loginSection.style.display = "flex"
+    let registerSection = document.getElementById("register-section")
+    registerSection.style.display = "none"
+
+})
+
+//mostrar senha
+
+function verSenhaLogin() {
+    let passwordLogin = document.querySelector(".password-login")
+
+    if (passwordLogin.getAttribute('type') == "password") {
+        passwordLogin.setAttribute('type', 'text');
+    }
+    else {
+        passwordLogin.setAttribute('type', 'password');
+    }
+
+}
+
+
+function verSenhaRegister() {
+
+    let passwordRegister1 = document.getElementById("password-register1")
+    let passwordRegister2 = document.getElementById("password-register2")
+
+    if (passwordRegister1.getAttribute('type') == "password") {
+        passwordRegister1.setAttribute('type', 'text');
+        passwordRegister2.setAttribute('type', 'text');
+    }
+    else {
+        passwordRegister1.setAttribute('type', 'password');
+        passwordRegister2.setAttribute('type', 'password');
+    }
+}
+
+
