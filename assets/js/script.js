4dfc03ed078ccf8b86cfc095317c0549b0cb03bb
carrrllos <caedu762@gmail.com>
2023-04-10 17:14:34 -0300

    add reg.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,31 +54,7 @@ function verSenhaRegister() {
 }
 
 
-function register() {
-
-    let passwordRegister1Value = document.getElementById("password-register1").value
-    let passwordRegister2Value = document.getElementById("password-register2").value
-
-    if (passwordRegister1Value != passwordRegister2Value) {
-        alert("senhas diferentes")
-    }
-    else if (passwordRegister1Value.length == 0 || passwordRegister2Value.length == 0) {
-        alert("digite uma senha")
-    }
-
-    else {
-        var hashPassword = 0;
-        // if the length of the string is 0, return 0
-        if (passwordRegister1Value.length == 0) return hashPassword;
-        for (i = 0; i < passwordRegister1Value.length; i++) {
-            ch = passwordRegister1Value.charCodeAt(i);
-            hashPassword = ((hashPassword << 5) - hashPassword) + ch;
-            hashPassword = hashPassword & hashPassword;
-        }
-        console.log(hashPassword);
-    }
 
 
-}
 
 
