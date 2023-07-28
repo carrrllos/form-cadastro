2e56442f335bbe47bcba121d8d8e811f809e3d0a
carrrllos <caedu762@gmail.com>
2023-07-28 19:08:28 -0300

    add page

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,6 +49,77 @@ function verSenhaRegister() {
     }
 }
 
+function registrar() {
+
+    let passwordRegister1Value = document.getElementById("password-register1").value
+    let passwordRegister2Value = document.getElementById("password-register2").value
+    let usernameRegisterValue = document.getElementById("username-register").value
+
+    if (passwordRegister1Value != passwordRegister2Value) {
+        alert("senhas diferentes")
+    }
+    else if (passwordRegister1Value.length == 0 || passwordRegister2Value.length == 0 || usernameRegisterValue == 0) {
+        alert("não deixe espaços vazios")
+    }
+
+    else {
+
+        var hashPasswordRegister = 0;
+
+        if (passwordRegister1Value.length == 0) return hashPasswordRegister;
+        for (i = 0; i < passwordRegister1Value.length; i++) {
+            ch = passwordRegister1Value.charCodeAt(i);
+            hashPasswordRegister = ((hashPasswordRegister << 5) - hashPasswordRegister) + ch;
+            hashPasswordRegister = hashPasswordRegister & hashPasswordRegister;
+
+        }
+        hashPasswordRegister = hashPasswordRegister.toString()
+        console.log(hashPasswordRegister);
+
+        if (localStorage.getItem(`${usernameRegisterValue}`)) {
+            alert("Username indisponível")
+        }
+        else {
+
+            localStorage.setItem(`${usernameRegisterValue}`, `${hashPasswordRegister}`)
+            alert("Usuário cadastrado")
+        }
+
+    }
+}
+
+function logar() {
+    let passwordLoginValue = document.getElementById("password-login").value
+    let usernameLoginValue = document.getElementById("username-login").value
+
+    if (passwordLoginValue == 0 || usernameLoginValue == 0) {
+        alert("não deixe espaços vazios")
+    }
+
+    var hashPasswordLogin = 0;
+
+    if (passwordLoginValue.length == 0) return hashPasswordLogin;
+    for (i = 0; i < passwordLoginValue.length; i++) {
+        ch = passwordLoginValue.charCodeAt(i);
+        hashPasswordLogin = ((hashPasswordLogin << 5) - hashPasswordLogin) + ch;
+        hashPasswordLogin = hashPasswordLogin & hashPasswordLogin;
+
+    }
+    hashPasswordLogin = hashPasswordLogin.toString()
+
+    if(localStorage.getItem(`${usernameLoginValue}`) != hashPasswordLogin || !localStorage.getItem(`${usernameLoginValue}`)){
+        alert("Usuário ou senha inválidos")
+    }
+
+    else{
+        window.location.href = "page.html";
+    }
+    
+}
+
+
+
+
 
 
 
